641fc2bbd9f25b90ef01825f38cc15e431bea7c8
Andre Mauricio Zelak <andre.zelak@gmail.com>
2024-04-10 18:52:45 -0300

    Monitoring: Installed Prometheus
    Signed-off-by: Andre Mauricio Zelak <andre.zelak@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const prometheus = require('express-prom-bundle');
 const routes = require('./routes');
 
 const app = express();
 
 // API
 app.use('/', routes);
+
+app.use(prometheus());
 
 module.exports = app;
